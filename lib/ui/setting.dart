6c1756a53912a2c21683386f2385ceaeacb7cf50
jacktnp <thanaponpkw@gmail.com>
2019-05-17 16:39:52 +0700

    :balloon: update

--- lib/ui/setting.dart
+++ lib/ui/setting.dart
@@ -17,18 +17,33 @@ class SettingState extends State<Setting> {
         title: Text("Settings"),
         centerTitle: true,
       ),
+      /*
       body: Container(
         //color: Colors.amber,
         padding: EdgeInsets.all(30.0),
         margin: EdgeInsets.all(16.0),
         width: 500.0,
-        height: 300.0,
+        height: 1000.0,
         child: Container(
           //color: Colors.teal,
-          child: FlatButton.icon(
-            icon: Icon(Icons.music_note),
-            label: Text('Background music'),
+          child: Text(
+            'Background music\n\n\nSFX Music\n\n\n! Notification\n\n! About',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 17.0,
+            ),
           ),
+        ),
+      ),
+      */
+      body: new Row(
+        children: <Widget>[
+          new IconButton(icon: Icon(Icons.add)),
+          new Text('\n\nBackground music\n\n', textAlign: TextAlign.center),
+        ],
+        Container(
+          new IconButton(icon: Icon(Icons.music_note)),
+          new Text('\n\nSFX music\n\n', textAlign: TextAlign.center),
         ),
       ),
     );
