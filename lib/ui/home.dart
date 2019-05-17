49f31350b7ed7be8994ec76c08f65b84fde233aa
Tinnapat Plangsri <pltinnapat@gmail.com>
2019-05-18 00:48:56 +0700

    update home.dart ⚡️

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -15,13 +15,14 @@ class Home extends StatefulWidget {
 
 class HomeState extends State<Home> {
   String _plantImage = "assets/LV0.png";
+  int _fullPerLvl = 1000;
   
 //=================================pedometer part==================================
   String _km = "0.0"; //distance
   String _totalKm = "0.0"; // Total distance
   int _stepCountValue = 0; //all Foot step record
   int  _remainStepCount = 0; //step that will be showed on screen
-  int plants = 0; //all lvl 5 plants of user
+  int _plants = 0; //all lvl 5 plants of user
   StreamSubscription<int> _subscription; // for pedometer package
   int _lvl = 0;
 
@@ -98,10 +99,10 @@ class HomeState extends State<Home> {
           padding: EdgeInsets.all(30.0),
           children: <Widget>[
             _tree(_plantImage),
-            _barnum(),
-            _bar(),
-            _bartxt(),
-            _distance(),
+            _barnum(_remainStepCount, _fullPerLvl),
+            _bar(_remainStepCount, _fullPerLvl),
+            _bartxt('การเจริญเติบโต'),
+            _distance(_km),
           ],
         ),
       ),
@@ -141,6 +142,8 @@ class HomeState extends State<Home> {
       stepCountValue = 0;
       _stepCountValue = stepCountValue;
       _km = "0.0";
+      _plants += 1;
+      _fullPerLvl = 1000;
     });
   }
   
@@ -149,62 +152,69 @@ class HomeState extends State<Home> {
     if (_stepCountValue < 1000){ 
       setState(() => _lvl = 0);
       setState(() => _remainStepCount = _stepCountValue);
+      setState(() => _fullPerLvl = 1000);
     } else if (_stepCountValue < 5000) { 
       setState(() => _lvl = 1);
       setState(() => _remainStepCount = _stepCountValue-1000);
+      setState(() => _fullPerLvl = 5000);
     } else if (_stepCountValue < 10000) { 
       setState(() => _lvl = 2); 
       setState(() => _remainStepCount = _stepCountValue-5000);
+      setState(() => _fullPerLvl = 10000);
     } else if (_stepCountValue < 50000) { 
       setState(() => _lvl = 3);
       setState(() => _remainStepCount = _stepCountValue-10000);
-    } else if (_stepCountValue < 100000) { 
+      setState(() => _fullPerLvl = 50000);
+    } else if (_stepCountValue < 100000) {
       setState(() => _lvl = 4);
       setState(() => _remainStepCount = _stepCountValue-50000);
+      setState(() => _fullPerLvl = 100000);
     } else { 
       setState(() => _lvl = 5);
       setState(() => _remainStepCount = _stepCountValue-100000);
     }
+    this._plantImage = "assets/LV$_lvl.png";
   }
 //=================================pedometer part==================================  
 }
 
-Widget _distance() {
+Widget _distance(km) {
   return Container(
     child: Padding(
       padding: EdgeInsets.fromLTRB(0.0, 25.0, 0, 0),
       child: Text(
-        'ระยะทางรวม\n\nXXX Km',
+        'ระยะทางรวม\n\n$km Km',
         textAlign: TextAlign.center
       ),
     ),
   );
 }
 
-Widget _barnum(){
+Widget _barnum(int remainStep, int fullPerLvl){
   return Text(
-      '500/500',
+      '$remainStep /$fullPerLvl',
       textAlign: TextAlign.center,
       style: TextStyle(color: Colors.black, fontSize: 14.0)
   );
 }
 
-Widget _bar() {
+Widget _bar(int remainStep, int fullPerLvl) {
+  double percentNum = remainStep/fullPerLvl;
   return Padding(
     padding: EdgeInsets.fromLTRB(80.0, 0, 0, 0),
     child: LinearPercentIndicator(
       width: 140.0,
       lineHeight: 14.0,
-      percent: 0.5,
+      percent: percentNum,
       backgroundColor: Colors.grey,
       progressColor: Colors.teal,
     ),
   );
 }
 
-Widget _bartxt(){
+Widget _bartxt(String txt){
   return Text(
-      'การเจริญเติบโต',
+      txt,
       textAlign: TextAlign.center,
       style: TextStyle(color: Colors.grey, fontSize: 10.0)
   );
