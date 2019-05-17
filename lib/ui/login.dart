c5c63e9a076bd72321ba174b2fce91d02153ee3a
jacktnp <thanaponpkw@gmail.com>
2019-05-17 14:11:50 +0700

    :balloon: update login&register

--- lib/ui/login.dart
+++ lib/ui/login.dart
@@ -11,71 +11,85 @@ class Login extends StatefulWidget {
 
 
 class LoginState extends State<Login> {
-  final emailField = TextField(
-          obscureText: false,
-          style: style,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              hintText: "Email",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        );
-        final passwordField = TextField(
-          obscureText: true,
-          style: style,
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-              hintText: "Password",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
-        );
-        final loginButon = Material(
-          elevation: 5.0,
-          borderRadius: BorderRadius.circular(30.0),
-          color: Color(0xff01A0C7),
-          child: MaterialButton(
-            minWidth: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-            onPressed: () {},
-            child: Text("Login",
-                textAlign: TextAlign.center,
-                style: style.copyWith(
-                    color: Colors.white, fontWeight: FontWeight.bold)),
-          ),
-        );
+  final _formKey = GlobalKey<FormState>();
 
-        return Scaffold(
-          body: Center(
-            child: Container(
-              color: Colors.white,
-              child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    SizedBox(
-                      height: 155.0,
-                      child: Image.asset(
-                        "assets/logo.png",
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    SizedBox(height: 45.0),
-                    emailField,
-                    SizedBox(height: 25.0),
-                    passwordField,
-                    SizedBox(
-                      height: 35.0,
-                    ),
-                    loginButon,
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                  ],
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 100, right: 60, left: 60),
+
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              Image.asset(
+                "resources/logo.png",
+                height: 150,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "User Id",
+                  hintText: "User Id",
+                  icon: Icon(Icons.people),
+                ),
+                keyboardType: TextInputType.text,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  if(value.isEmpty){
+                    return "กรุณาระบุ user or password";
+                  }
+                  if(value.toLowerCase() == "admin"){
+                    return "user or password ไม่ถูกต้อง";
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Password",
+                  hintText: "Password",
+                  icon: Icon(Icons.lock),
+                ),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  if(value.isEmpty){
+                    return "กรุณาระบุ user or password";
+                  }
+                  if(value == "admin"){
+                    return "user or password ไม่ถูกต้อง";
+                  }
+                },
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15),
+                child: RaisedButton(
+                  child: Text("LOGIN"),
+                  onPressed: (){
+                    if(_formKey.currentState.validate()){
+                      Navigator.pushNamed(context, '/home');
+                    }
+                  },
                 ),
               ),
-            ),
+              Container(
+                child: FlatButton(
+                  child: Text("Register New Account"),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/register');
+                  },
+                  textColor: Colors.blue,
+                ),
+                alignment: Alignment.bottomRight,
+              ),
+            ],
           ),
-        );
+        )
+      )
+    );
+  }
 }
--- lib/ui/register.dart
+++ lib/ui/register.dart
@@ -11,13 +11,93 @@ class Register extends StatefulWidget {
 
 
 class RegisterState extends State<Register> {
+  final _formKey = GlobalKey<FormState>();
+  var password_1 = '';
+  var password_2 = '';
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text("Register"),
         centerTitle: true,
       ),
+      body: Container(
+        padding: EdgeInsets.only(top: 30, right: 20, left: 20),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Email",
+                  hintText: "Email",
+                  icon: Icon(Icons.email),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  if(value.isEmpty){
+                    return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                  }
+                  if(value.toLowerCase() == "admin"){
+                    return "user นี้มีอยู่ในระบบแล้ว";
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Password",
+                  hintText: "Password",
+                  icon: Icon(Icons.lock),
+                ),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  this.password_1 = value;
+                  if(value.isEmpty){
+                    return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  //labelText: "Password",
+                  hintText: "Password",
+                  icon: Icon(Icons.lock),
+                ),
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                onSaved: (value) => print(value),
+                validator: (value){
+                  this.password_2 = value;
+                  if(value.isEmpty){
+                    
+                    return "กรุณาระบุข้อมูลให้ครบถ้วน";
+                  }
+                  if(password_1 != password_2){
+                    print(password_1);
+                    
+                    print(password_2);
+                    return "รหัสผ่านไม่ตรงกัน";
+                  }
+                },
+              ),
+              Container(
+                  padding: EdgeInsets.only(top: 15),
+                  child: RaisedButton(
+                    child: Text("CONTINUE"),
+                    onPressed: (){
+                      if(_formKey.currentState.validate()){
+                        Navigator.pushNamed(context, '/');
+                      }
+                    },
+                  ),
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
